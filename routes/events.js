10b42ad6b58c1f0a128874c57e8f7d13da54e264
Herdis Maria <herdismaria@herdiss-mbp.home>
2017-09-18 11:06:29 +0200

    create an event endpoint (POST events)

--- routes/events.js
+++ routes/events.js
@@ -1,15 +1,23 @@
 let express = require('express');
 let router = express.Router();
+const uuidv4 = require('uuid/v4');
 let events = require('../data')
 
 /* GET events listing. */
 router.get('/', (req, res, next) => res.send(events))
 
 router.post('/', (req, res, next ) => {
-  events = {
-    ...events,
-    
+  const id = uuidv4()
+  console.log(req)
+  const newEvent = {
+    name: req.body.name,
+    description: req.body.description,
+    date: req.body.date,
+    id: id
   }
+  events[id] = newEvent
+
+  res.send(newEvent)
 })
 
 router.get('/:id', (req, res, next) =>
