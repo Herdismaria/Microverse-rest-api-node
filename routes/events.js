c951fc6f460023f6e4eb4c66a06addbaecde5690
Herdis Maria <herdismaria@herdiss-macbook-pro.home>
2017-10-02 11:05:54 +0200

    Added test for /POST events route

--- routes/events.js
+++ routes/events.js
@@ -1,57 +1,59 @@
 let express = require('express');
 let router = express.Router();
 const uuidv4 = require('uuid/v4');
-let events = require('../data')
+let events = require('../data');
 
 /* GET */
-router.get('/', (req, res, next) => res.send(events))
+router.get('/', (req, res, next) => res.send(events));
 
 router.get('/:id', (req, res, next) => {
-  let event = events[req.params.id]
-  if(!event) {
-    return res.sendStatus(404)
+  let event = events[req.params.id];
+  if (!event) {
+    return res.sendStatus(404);
   }
-  res.send(event)
-})
+  res.send(event);
+});
 
 router.post('/', (req, res, next) => {
-  const id = uuidv4()
+  const id = uuidv4();
   let newEvent = {
     id,
     title: req.body.title,
     description: req.body.description,
-    date: req.body.date
-  }
+    date: req.body.date,
+  };
 
-  events[id] = newEvent
-  res.send(newEvent)
-})
+  events[id] = newEvent;
+  res.status(201).send(newEvent);
+});
 
 router.patch('/:id', (req, res, next) => {
-  let event = events[req.params.id]
-  if(!event) {
-    return res.sendStatus(404)
+  let event = events[req.params.id];
+  if (!event) {
+    return res.sendStatus(404);
   }
 
   let modEvent = {
     id: event.id,
     title: req.body.title ? req.body.title : event.title,
-    description: req.body.description ? req.body.description : event.description,
-    date: req.body.date ? req.body.date : event.date
-  }
+    description: req.body.description
+      ? req.body.description
+      : event.description,
+    date: req.body.date ? req.body.date : event.date,
+  };
 
-  events[req.params.id] = modEvent
-  res.send(modEvent)
-})
+  events[req.params.id] = modEvent;
+  res.send(modEvent);
+});
 
 router.delete('/:id', (req, res, next) => {
-    let event = events[req.params.id]
-    if(!event) {
-        return res.sendStatus(404)
-    }
+  let event = events[req.params.id];
+  if (!event) {
+    return res.sendStatus(404);
+  }
 
-    delete events[req.params.id]
-    res.send()
-})
+  delete events[req.params.id];
+  res.send();
+});
 
 module.exports = router;
--- test/test_events.js
+++ test/test_events.js
@@ -12,24 +12,47 @@ let should = chai.should();
 
 chai.use(chaiHttp);
 //Our parent block
-// describe('Books', () => {
-//     beforeEach((done) => { //Before each test we empty the database
-//         Book.remove({}, (err) => {
-//             done();
-//         });
-//     });
-/*
-* Test the /GET events list route
-*/
-describe('/GET event', () => {
-    it('it should GET all the events', (done) => {
-        chai.request(server)
-            .get('/events')
-            .end((err, res) => {
-                res.should.have.status(200);
-                res.body.should.be.a('object');
-                res.body.should.not.have.property('errors');
-                done();
-            });
+describe('Events', () => {
+  //Test the /GET events list route
+  describe('/GET event', () => {
+    it('it should GET all the events', done => {
+      chai
+        .request(server)
+        .get('/events')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a('object');
+          res.body.should.not.have.property('errors');
+          done();
+        });
     });
+  });
+
+  describe('/POST event', () => {
+    it('it should create a new event', done => {
+      let event = {
+        title: 'Test title',
+        description: 'Test description',
+        date: '12.12.2012',
+      };
+
+      chai
+        .request(server)
+        .post('/events')
+        .send(event)
+        .end((err, res) => {
+          res.should.have.status(201);
+          res.body.should.be.a('object');
+          res.body.should.have.property('title');
+          res.body.should.have.property('description');
+          res.body.should.have.property('date');
+          res.body.should.have.property('id');
+          res.body.title.should.equal('Test title');
+          res.body.description.should.equal('Test description');
+          res.body.date.should.equal('12.12.2012');
+          res.body.should.not.have.property('errors');
+          done();
+        });
+    });
+  });
 });
