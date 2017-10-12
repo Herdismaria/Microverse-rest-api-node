c383b15b0016cb6e1e7e7236197c6877eccaf585
Herdis Maria <herdismaria@herdiss-macbook-pro.home>
2017-10-12 10:22:00 +0200

    Added mongodb and connected to database

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
-const express = require('express')
-var bodyParser = require('body-parser')
+const express = require('express');
+var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var events = require('./routes/events');
-
-const app = express()
+var MongoClient = require('mongodb').MongoClient,
+  assert = require('assert');
+// Connection URL
+var url = 'mongodb://localhost:27017/microverse';
+const app = express();
 
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -11,8 +14,16 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/events', events);
 app.use('/', index);
 
-if(!module.parent) {
-    app.listen(3000, () => console.log('Example app listening on port 3000!'));
+// Use connect method to connect to the server
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log('Connected successfully to server');
+  const eventsRef = db.collection('events');
+  db.close();
+});
+
+if (!module.parent) {
+  app.listen(3000, () => console.log('Example app listening on port 3000!'));
 }
 
 module.exports = app; // for testing
