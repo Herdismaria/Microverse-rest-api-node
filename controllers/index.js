5b1b33e866e19bdb834e149eebcd225599b2df94
Daria Chernobaieva <daria.chernobaieva@gmail.com>
2017-11-07 11:56:05 +0100

    Added authentication to get events. Checked passwords directly in passport.use.

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+let passport = require('passport');
 
 /* GET home page. */
 router.get('/', (req, res, next) => res.send('Hello World!'));
