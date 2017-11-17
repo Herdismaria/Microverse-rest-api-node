cb143e66a67b892771a9d5f61b051da704be76c8
Herdis Maria <herdismaria@herdiss-macbook-pro.home>
2017-11-17 10:42:55 +0100

    Tests are passing

--- test/test_events.js
+++ test/test_events.js
@@ -18,7 +18,6 @@ const user_params = {
   email: 'test@email.com',
   fullname: 'Test name',
 };
-const auth_user = new User(user_params);
 
 //Our parent block
 describe('Events', () => {
@@ -40,7 +39,7 @@ describe('Events', () => {
   //Test the /GET events list route
   describe('/GET events', () => {
     it('it should GET all the events', done => {
-      auth_user.save((err, user) => {
+      new User(user_params).save((err, user) => {
         chai
           .request(server)
           .get('/events')
@@ -62,7 +61,7 @@ describe('Events', () => {
         description: 'Test description',
         date: new Date('2012-12-12'),
       };
-      auth_user.save((err, user) => {
+      new User(user_params).save((err, user) => {
         chai
           .request(server)
           .post('/events')
@@ -85,7 +84,7 @@ describe('Events', () => {
     });
   });
 
-  /*describe('/GET single event', () => {
+  describe('/GET single event', () => {
     it('it should get single event', done => {
       let event = new Event({
         title: 'Test title',
@@ -93,7 +92,7 @@ describe('Events', () => {
         date: new Date('2016-12-12'),
       });
       event.save((err, event) => {
-        auth_user.save((err, user) => {
+        new User(user_params).save((err, user) => {
           chai
             .request(server)
             .get('/events/' + event._id)
@@ -118,155 +117,178 @@ describe('Events', () => {
 
     it('it should fail get single event cause of non-existent event', done => {
       const nonExistentEventId = 'non-existent-uuid';
-      chai
-        .request(server)
-        .get('/events/' + nonExistentEventId)
-        .end((err, res) => {
-          res.should.have.status(404);
-          res.error.should.not.equal(null);
-          done();
-        });
+      new User(user_params).save((err, user) => {
+        chai
+          .request(server)
+          .get('/events/' + nonExistentEventId)
+          .auth(user.username, user.password)
+          .end((err, res) => {
+            res.should.have.status(404);
+            res.error.should.not.equal(null);
+            done();
+          });
+      });
     });
-  });*/
+  });
 
-  //   describe('/PATCH event', () => {
-  //     it('it should update the whole event', done => {
-  //       let event = new Event({
-  //         title: 'Test title',
-  //         description: 'Test description',
-  //         date: new Date('2016-12-12'),
-  //       });
-  //       let updated_event_params = {
-  //         title: 'Updated event',
-  //         description: 'Updated description',
-  //         date: new Date('2013-12-12'),
-  //       };
-  //
-  //       event.save((err, updated_event) => {
-  //         chai
-  //           .request(server)
-  //           .patch('/events/' + event._id)
-  //           .send(updated_event_params)
-  //           .end((err, res) => {
-  //             res.should.have.status(200);
-  //             res.body.should.be.a('object');
-  //             res.body.should.have.property('title');
-  //             res.body.should.have.property('description');
-  //             res.body.should.have.property('date');
-  //             res.body.should.have.property('_id');
-  //             res.body._id.should.equal(event._id.toString());
-  //             res.body.title.should.equal(updated_event_params.title);
-  //             res.body.description.should.equal(updated_event_params.description);
-  //             res.body.date.should.equal(updated_event_params.date.toISOString());
-  //             res.body.should.not.have.property('errors');
-  //             done();
-  //           });
-  //       });
-  //     });
-  //
-  //     it('it should update the title of the event', done => {
-  //       let event = new Event({
-  //         title: 'Test title',
-  //         description: 'Test description',
-  //         date: new Date('2016-12-12'),
-  //       });
-  //       let updated_event_params = {
-  //         title: 'Updated only title',
-  //       };
-  //       event.save((err, updated_event) => {
-  //         chai
-  //           .request(server)
-  //           .patch('/events/' + event._id)
-  //           .send(updated_event_params)
-  //           .end((err, res) => {
-  //             res.should.have.status(200);
-  //             res.body.should.be.a('object');
-  //             res.body.should.have.property('title');
-  //             res.body.should.have.property('description');
-  //             res.body.should.have.property('date');
-  //             res.body.should.have.property('_id');
-  //             res.body._id.should.equal(event._id.toString());
-  //             res.body.title.should.equal(updated_event_params.title);
-  //             res.body.description.should.equal(event.description);
-  //             res.body.date.should.equal(event.date.toISOString());
-  //             res.body.should.not.have.property('errors');
-  //             done();
-  //           });
-  //       });
-  //     });
-  //
-  //     it('it should fail event update cause of non-existent event', done => {
-  //       const nonExistentEventId = 'non-existent-uuid';
-  //       let updated_event_params = {
-  //         title: 'Updated title',
-  //       };
-  //       chai
-  //         .request(server)
-  //         .patch('/events/' + nonExistentEventId)
-  //         .send(updated_event_params)
-  //         .end((err, res) => {
-  //           res.should.have.status(404);
-  //           done();
-  //         });
-  //     });
-  //   });
-  //
-  //   describe('/DELETE event', () => {
-  //     it('it should delete single event', done => {
-  //       let event = new Event({
-  //         title: 'Test title',
-  //         description: 'Test description',
-  //         date: new Date('2016-12-12'),
-  //       });
-  //       event.save((err, event) => {
-  //         chai
-  //           .request(server)
-  //           .delete('/events/' + event._id)
-  //           .end((err, res) => {
-  //             res.should.have.status(200);
-  //             res.body.should.be.empty;
-  //             res.body.should.not.have.property('errors');
-  //             done();
-  //           });
-  //       });
-  //     });
-  //
-  //     it('it should fail event delete cause of non-existent event', done => {
-  //       const nonExistentEventId = 2017;
-  //       chai
-  //         .request(server)
-  //         .delete('/events/' + nonExistentEventId)
-  //         .end((err, res) => {
-  //           res.should.have.status(404);
-  //           done();
-  //         });
-  //     });
-  //   });
-  //
-  //   // Search events by title
-  //   describe('/GET events by title', () => {
-  //         it('it should search all the events by title', done => {
-  //             let event = new Event({
-  //                 title: 'Search event',
-  //                 description: 'Search event description',
-  //                 date: new Date('2016-12-12')
-  //             });
-  //             event.save((err, event) => {
-  //                 chai
-  //                     .request(server)
-  //                     .get('/events/search?title=' + event.title)
-  //                     .end((err, res) => {
-  //                         res.should.have.status(200);
-  //                         res.body.should.be.a('array');
-  //                         res.body[0]._id.should.equal(event._id.toString());
-  //                         res.body[0].title.should.equal(event.title);
-  //                         res.body[0].description.should.equal(event.description);
-  //                         res.body[0].date.should.equal(event.date.toISOString());
-  //                         res.body.should.not.have.property('errors');
-  //                         done();
-  //                     });
-  //             });
-  //
-  //         });
-  //     });
+  describe('/PATCH event ', () => {
+    it('it should update the whole event', done => {
+      let event = new Event({
+        title: 'Test title',
+        description: 'Test description',
+        date: new Date('2016-12-12'),
+      });
+      let updated_event_params = {
+        title: 'Updated event',
+        description: 'Updated description',
+        date: new Date('2013-12-12'),
+      };
+      new User(user_params).save((err, user) => {
+        event.save((err, updated_event) => {
+          chai
+            .request(server)
+            .patch('/events/' + event._id)
+            .send(updated_event_params)
+            .auth(user.username, user.password)
+            .end((err, res) => {
+              res.should.have.status(200);
+              res.body.should.be.a('object');
+              res.body.should.have.property('title');
+              res.body.should.have.property('description');
+              res.body.should.have.property('date');
+              res.body.should.have.property('_id');
+              res.body._id.should.equal(event._id.toString());
+              res.body.title.should.equal(updated_event_params.title);
+              res.body.description.should.equal(
+                updated_event_params.description
+              );
+              res.body.date.should.equal(
+                updated_event_params.date.toISOString()
+              );
+              res.body.should.not.have.property('errors');
+              done();
+            });
+        });
+      });
+    });
+
+    it('it should update the title of the event', done => {
+      let event = new Event({
+        title: 'Test title',
+        description: 'Test description',
+        date: new Date('2016-12-12'),
+      });
+      let updated_event_params = {
+        title: 'Updated only title',
+      };
+      new User(user_params).save((err, user) => {
+        event.save((err, updated_event) => {
+          chai
+            .request(server)
+            .patch('/events/' + event._id)
+            .send(updated_event_params)
+            .auth(user.username, user.password)
+            .end((err, res) => {
+              res.should.have.status(200);
+              res.body.should.be.a('object');
+              res.body.should.have.property('title');
+              res.body.should.have.property('description');
+              res.body.should.have.property('date');
+              res.body.should.have.property('_id');
+              res.body._id.should.equal(event._id.toString());
+              res.body.title.should.equal(updated_event_params.title);
+              res.body.description.should.equal(event.description);
+              res.body.date.should.equal(event.date.toISOString());
+              res.body.should.not.have.property('errors');
+              done();
+            });
+        });
+      });
+    });
+
+    it('it should fail event update cause of non-existent event', done => {
+      const nonExistentEventId = 'non-existent-uuid';
+      let updated_event_params = {
+        title: 'Updated title',
+      };
+      new User(user_params).save((err, user) => {
+        chai
+          .request(server)
+          .patch('/events/' + nonExistentEventId)
+          .send(updated_event_params)
+          .auth(user.username, user.password)
+          .end((err, res) => {
+            res.should.have.status(404);
+            done();
+          });
+      });
+    });
+  });
+
+  describe('/DELETE event', () => {
+    it('it should delete single event', done => {
+      let event = new Event({
+        title: 'Test title',
+        description: 'Test description',
+        date: new Date('2016-12-12'),
+      });
+      new User(user_params).save((err, user) => {
+        event.save((err, event) => {
+          chai
+            .request(server)
+            .delete('/events/' + event._id)
+            .auth(user.username, user.password)
+            .end((err, res) => {
+              res.should.have.status(200);
+              res.body.should.be.empty;
+              res.body.should.not.have.property('errors');
+              done();
+            });
+        });
+      });
+    });
+
+    it('it should fail event delete cause of non-existent event', done => {
+      const nonExistentEventId = 2017;
+      new User(user_params).save((err, user) => {
+        chai
+          .request(server)
+          .delete('/events/' + nonExistentEventId)
+          .auth(user.username, user.password)
+          .end((err, res) => {
+            res.should.have.status(404);
+            done();
+          });
+      });
+    });
+  });
+
+  // Search events by title
+  describe('/GET events by title', () => {
+    it('it should search all the events by title', done => {
+      let event = new Event({
+        title: 'Search event',
+        description: 'Search event description',
+        date: new Date('2016-12-12'),
+      });
+      new User(user_params).save((err, user) => {
+        event.save((err, event) => {
+          chai
+            .request(server)
+            .get('/events/search?title=' + event.title)
+            .auth(user.username, user.password)
+            .end((err, res) => {
+              res.should.have.status(200);
+              res.body.should.be.a('array');
+              res.body[0]._id.should.equal(event._id.toString());
+              res.body[0].title.should.equal(event.title);
+              res.body[0].description.should.equal(event.description);
+              res.body[0].date.should.equal(event.date.toISOString());
+              res.body.should.not.have.property('errors');
+              done();
+            });
+        });
+      });
+    });
+  });
 });
